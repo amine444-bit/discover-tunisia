5e7f50ec81773a545055f63465ce49b9a1198423
amine444-bit <MohamedAmine.MHIMDI@esprit.tn>
2026-03-28 18:47:49 +0100

    fix: mise à jour des fichiers HTML et readme

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const form = document.getElementById("contactForm");
 
-    if (form) {
-        form.addEventListener("submit", function (e) {
-            e.preventDefault();
-            alert("Message envoyé avec succès !");
-        });
+    form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nom = document.getElementById("nom").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (!nom || !email || !message) {
+        alert("Veuillez remplir tous les champs !");
+        return;
+    }
+
+    alert("Message envoyé avec succès !");
+});
     }
 
 });
